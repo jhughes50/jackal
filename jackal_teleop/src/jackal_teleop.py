eab9c2013c798699b91e40ae930edaae02a2d55d
Akhilesh <akoo09@seas.upenn.edu>
2020-01-13 21:05:48 -0500

    Files modified to run nav stack on jackal

---
+++ jackal_teleop/src/jackal_teleop.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+class JackalTeleop:
+    def __init__(self):
+        self.twist_buf_ = Twist()
+        self.is_auto_ = False
+
+        self.joy_sub_ = rospy.Subscriber('joy', Joy, self.joy_cb)
+        self.twist_sub_ = rospy.Subscriber('twist_auto', Twist, self.twist_cb)
+        self.twist_pub_ = rospy.Publisher('twist_out', Twist, queue_size=1)
+        
+        self.pub_timer_ = rospy.Timer(rospy.Duration(0.1), self.pub_cb)
+
+    def pub_cb(self, event):
+        if not self.is_auto_:
+            self.twist_pub_.publish(self.twist_buf_)
+
+    def joy_cb(self, msg):
+        self.twist_buf_.linear.x = msg.axes[3]
+        self.twist_buf_.angular.z = msg.axes[2]
+        self.is_auto_ = msg.axes[4] > 0
+
+    def twist_cb(self, msg):
+        if self.is_auto_:
+            self.twist_pub_.publish(msg)
+
+if __name__=='__main__':
+    rospy.init_node('jackal_teleop')
+    jackal_telep = JackalTeleop()
+    rospy.spin()
